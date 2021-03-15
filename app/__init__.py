943ac8f24c31a9cd73efa04f44f968012073f9a1
Andreas Fridh <andreas.frid@statkraft.com>
2021-03-15 09:39:25 +0100

    Moving to object structure with calls from proof of concept

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ import math
 import numpy as np;
 import matplotlib 
 
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 # Analysis reponse class, variable names should be documents enough to understand this.
 class analysisResponse():
@@ -18,20 +18,34 @@ class analysisResponse():
         self.largest_blob_pos_x = largest_blob_pos_x
         self.largest_blob_pos_y = largest_blob_pos_y
 
+class images():
+    def __init__(self,img_raw=None,img_bw=None,img_mask=None,img_analysed=None):
+        self.raw = img_raw
+        self.bw = img_bw
+        self.mask = img_mask
+        self.analysed = img_analysed
+
+images = images()
 
 @app.route('/run_analysis')
 def run_analysis():
     
     result = analysisResponse()
-    
+
     result = peat_detector()
-        
+    
     return """
     
-    <h1>Blööbs found: """ + "hej" + """</h1>
+    Blob found, Max Area: """ + str(result.largest_blob_size) + """
+
 
     """
-    
+@app.route('/grab_img')
+def grab_img():
+    if file_write_result() == True: 
+        return send_from_directory("Result.png", "")
+    else: 
+        return "Image not grabbed ok"
 
 # 
 # Attributes:
@@ -66,73 +80,87 @@ def apply_text(img,x,y,text):
     
 
 def peat_detector():
-    grab_img()
-    
-    result = analysisResponse()
+    print("Peat detector running:")
+    if grab_img() == True :
+        print("Running analysis")
+        result = analysisResponse()
 
-    params = cv2.SimpleBlobDetector_Params()
+        params = cv2.SimpleBlobDetector_Params()
 
-    params.filterByArea = True
-    params.minArea = 100
-    params.maxArea = 100000
-    params.filterByCircularity = False
-    params.filterByColor = False
-    params.filterByConvexity = False
-    params.filterByInertia = False
+        params.filterByArea = True
+        params.minArea = 100
+        params.maxArea = 100000
+        params.filterByCircularity = False
+        params.filterByColor = False
+        params.filterByConvexity = False
+        params.filterByInertia = False
 
-    detector = cv2.SimpleBlobDetector_create(params)
+        detector = cv2.SimpleBlobDetector_create(params)
 
-    keypoints = detector.detect(im_masked)
+        keypoints = detector.detect(images.mask)
 
-    im_with_keypoints = cv2.drawKeypoints(img_in, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        images.analysed = cv2.drawKeypoints(images.raw, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    if keypoints:
-        print("Blobs FOUND")
-        print("Number of detected blobs: ", len(keypoints))
+        if keypoints:
+            print("Blobs FOUND")
+            print("Number of detected blobs: ", len(keypoints))
 
-        largest_blob_size   = 0
-        largest_blob_index  = 0
-        largest_blob_pos_x  = 0
-        largest_blob_pos_y  = 0
-        count = 0
+            largest_blob_size   = 0
+            largest_blob_index  = 0
+            largest_blob_pos_x  = 0
+            largest_blob_pos_y  = 0
+            count = 0
 
-        for keypoints in keypoints:
-            x = keypoints.pt[0]
-            y = keypoints.pt[1]
-            s = keypoints.size
-            
-            if keypoints.size > largest_blob_size:
-                largest_blob_size = keypoints.size
-                largest_blob_index = count
-                largest_blob_pos_x = x
-                largest_blob_pos_y = y
+            for keypoints in keypoints:
+                x = keypoints.pt[0]
+                y = keypoints.pt[1]
+                s = keypoints.size
                 
-            #print("Blob found at size: ", s)
-            #print("Index at: ", count)
+                if keypoints.size > largest_blob_size:
+                    largest_blob_size = keypoints.size
+                    largest_blob_index = count
+                    largest_blob_pos_x = x
+                    largest_blob_pos_y = y
+                    
+                #print("Blob found at size: ", s)
+                #print("Index at: ", count)
+                
+                count = count + 1
+
+            images.analysed = cv2.circle(images.analysed,( int(largest_blob_pos_x), int(largest_blob_pos_y)),int(largest_blob_size*1.2/2),(255,0,0),10)
+
+            print("Larges blob size: ", largest_blob_size)
+            print("Larges X: ", largest_blob_pos_x)
+            print("Larges Y: ", largest_blob_pos_y)
+            #print("Larges blob index: ", largest_blob_index)
             
-            count = count + 1
 
-        im_with_keypoints = cv2.circle(im_with_keypoints,( int(largest_blob_pos_x), int(largest_blob_pos_y)),int(largest_blob_size*1.2/2),(255,0,0),10)
+            result.blobsfound = True
+            result.largest_blob_size = largest_blob_size
+            result.largest_blob_pos_x = largest_blob_pos_x
+            result.largest_blob_pos_y = largest_blob_pos_y
 
-        img_out = im_with_keypoints
-
-        print("Larges blob size: ", largest_blob_size)
-        print("Larges X: ", largest_blob_pos_x)
-        print("Larges Y: ", largest_blob_pos_y)
-        #print("Larges blob index: ", largest_blob_index)
+        else: 
+            print("No blobs found")
+            result.blobsfound = False
         
-
-        result.blobsfound = True
-        result.largest_blob_size = largest_blob_size
-        result.largest_blob_pos_x = largest_blob_pos_x
-        result.largest_blob_pos_y = largest_blob_pos_y
-
+        return result
     else: 
-        print("No blobs found")
-        result.blobsfound = False
-    
-    return result
+        return None
 
+def file_write_result():
+    if grab_img() == True: 
+        filename = "result.png"
+
+        fig=plt.figure(figsize=(25, 25))
+        
+        plt.imshow(images.bw,cmap='gist_gray')
+
+        plt.savefig(filename) 
+
+        return True
+    else:
+        return False
         
 def write_report():  
     now = datetime.now()
@@ -165,12 +193,15 @@ def write_report():
     plt.show()
 
 def grab_img():
-    im_color = cv2.imread("test.jpg")
-    im = cv2.cvtColor(im_color, cv2.COLOR_BGR2RGB)
 
-    mask = cv2.imread("mask.jpg", 0)
+    images.raw = cv2.imread("test.jpg")
+    images.bw = cv2.cvtColor(images.raw , cv2.COLOR_BGR2RGB)
+    images.mask = cv2.imread("mask.jpg", 0)
 
-    im_masked = cv2.bitwise_and(im, im, mask = mask)
+    images.mask = cv2.bitwise_and(images.bw,images.bw,  mask = images.mask)
 
-if __name__ == '__main__':
-    app.run()
+    return True
+
+
+#if __name__ == '__main__':
+#   app.run()

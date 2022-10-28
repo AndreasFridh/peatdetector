1f591f26570b8589ce764eb802e4c826fe578206
Andreas Fridh <andreas@pyramind.se>
2022-10-28 09:55:30 +0200

    flip

--- app/devtools.py
+++ app/devtools.py
@@ -29,6 +29,7 @@ with Vimba.get_instance() as vimba:
 
     with cams[0] as cam:
         # print(cam.ChunkOffsetX())
+        cam.ReverseY(True)
         cam.ExposureAuto.set("Once")
         #cam.ExposureTime.set(50000)
 

5d8f80d02594bfa257b42bd75fe12ac638e33b96
AndreasFridh <46330513+AndreasFridh@users.noreply.github.com>
2023-08-31 13:43:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from vimba import *
 app = Flask(__name__)
 port = 5555
 
+# Class to store analysis response
 class AnalysisResponse:
     def __init__(self, blobs_found=False, largest_blob_size=0, largest_blob_pos=(0, 0), exposure_time=0):
         self.blobs_found = blobs_found    
@@ -15,6 +16,7 @@ class AnalysisResponse:
         self.largest_blob_pos = largest_blob_pos
         self.exposure_time = exposure_time
 
+# Class to store various images
 class Images:
     def __init__(self, raw=None, bw=None, mask=None, masked=None, analysed=None, filtered=None):
         self.raw = raw
@@ -73,10 +75,12 @@ def exp_dec():
 
 # Similar routes for exp_inc, exp_auto, exp_set...
 
+# Apply text to an image at specified position
 def apply_text(img_input, x, y, text_input):
     cv2.putText(img=img_input, text=text_input, fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                 org=(x, y), fontScale=2, thickness=10, color=(255, 255, 255))
 
+# Peat detection function
 def peat_detector():
     if grab_masked():
         params = cv2.SimpleBlobDetector_Params()
@@ -86,18 +90,21 @@ def peat_detector():
         keypoints = detector.detect(images.masked)
         # Rest of the analysis...
 
+# Report generation function
 def write_report():
     result = peat_detector()
     now = datetime.now()
     text = 'Tracked target: ' + now.strftime("%Y %m %d T%H-%M-%S")
     # Rest of the report generation...
 
+# Grab masked image
 def grab_masked():
     if grab_camera_frame():
         # Rest of the image processing...
         return True
     return False
 
+# Grab camera frame and convert
 def grab_camera_frame():
     with Vimba.get_instance() as vimba:
         cams = vimba.get_all_cameras()
@@ -105,8 +112,10 @@ def grab_camera_frame():
             frame = cam.get_frame()
             frame.convert_pixel_format(PixelFormat.Mono8)
             cv2.imwrite('frame.jpg', frame.as_opencv_image())
+            frame.release() 
             return True
 
+# Get current exposure time
 def camera_ctrl_exp_show():
     with Vimba.get_instance() as vimba:
         cams = vimba.get_all_cameras()
@@ -117,6 +126,7 @@ def camera_ctrl_exp_show():
 
 # Similar functions for camera control...
 
+# Send image with appropriate MIME type
 def send_image(image_filename):
     return send_file(image_filename, mimetype='image/jpg', download_name='snapshot.jpg')
 

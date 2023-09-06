cf0bfb6abe2a45a25ea69288c8e9965c501aed2a
AndreasFridh <46330513+AndreasFridh@users.noreply.github.com>
2023-09-06 13:13:45 +0200

    Update run.sh

--- assets/run.sh
+++ assets/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ehco "Installing VIMBA USB TTL env's"
-./opt/Vimba_4_0/VimbaUSBTL/SetGenTLPath.sh
+export GENICAM_GENTL32_PATH=:/opt/Vimba_4_0/VimbaUSBTL/CTI/arm_32bit
 echo "Pulling latest code from repo"
 cd /home/pi/git/peat_detector
+git stash
 git pull
+chmod 777 asset/run.sh
 echo "Starting flask app"
 cd app
 flask --app main run --host=0.0.0.0

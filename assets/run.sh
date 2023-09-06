1f554af55880a62eaa3a2e16d42ec5b094adb16f
AndreasFridh <46330513+AndreasFridh@users.noreply.github.com>
2023-09-06 13:19:14 +0200

    Update run.sh

--- assets/run.sh
+++ assets/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 ehco "Installing VIMBA USB TTL env's"
 export GENICAM_GENTL32_PATH=:/opt/Vimba_4_0/VimbaUSBTL/CTI/arm_32bit
-echo "Pulling latest code from repo"
-cd /home/pi/git/peat_detector
-git stash
-git pull
-chmod 777 asset/run.sh
+pip install MatPlotLib numpy VimbaPython
+echo 
+cd /home/pi/git/peat_detector/
 echo "Starting flask app"
 cd app
 flask --app main run --host=0.0.0.0
